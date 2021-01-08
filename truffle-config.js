eafb2efe32a6b397e6d2473c8c6227b520da1cf8
kalo <[natha20k@uregina.ca]>
2021-01-07 20:14:24 -0600

    installed hd wallet rpovider

--- truffle-config.js
+++ truffle-config.js
@@ -4,9 +4,9 @@ require('babel-polyfill');
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*" // Match any network id
+      host: "127.0.0.1", // Localhost (default: none)
+      port: 8545, // Standard Ethereum port (default: none)
+      network_id: "*" // Any network (default: none)
     },
   },
   contracts_directory: './src/contracts/',
