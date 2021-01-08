0db0c29b4478357c3c4903c001719946bae36bfe
kalo <[natha20k@uregina.ca]>
2021-01-08 02:15:20 -0600

    Updates

---
+++ precache-manifest.de0f99c12665d8e3cc5d405b3eb08272.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "06aa3e3ef75b16a0bdbc",
+    "url": "/token-farm/static/css/main.20028c1a.chunk.css"
+  },
+  {
+    "revision": "06aa3e3ef75b16a0bdbc",
+    "url": "/token-farm/static/js/main.06aa3e3e.chunk.js"
+  },
+  {
+    "revision": "8a15121cb41a6675f11b",
+    "url": "/token-farm/static/css/1.8c06e4eb.chunk.css"
+  },
+  {
+    "revision": "8a15121cb41a6675f11b",
+    "url": "/token-farm/static/js/1.8a15121c.chunk.js"
+  },
+  {
+    "revision": "b281d237ee20e71d5cae",
+    "url": "/token-farm/static/js/runtime~main.b281d237.js"
+  },
+  {
+    "revision": "86f6235d305b0f1bdbbd5459e1346f3b",
+    "url": "/token-farm/index.html"
+  }
+];
